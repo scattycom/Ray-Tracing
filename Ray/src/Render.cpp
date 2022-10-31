739c2b5a9d4e1e943d78cf926c5f7b3b6ff6d8ce
scattycom <48954539+scattycom@users.noreply.github.com>
2022-11-01 00:21:26 +0800

    加了灯光和其他改进
    1.位运算
    2.直接给像素赋值
    3.屏幕横纵比的问题

--- Ray/src/Render.cpp
+++ Ray/src/Render.cpp
@@ -1,5 +1,21 @@
 #include"render.h"
 
+namespace utils
+{
+	static uint32_t ConvertToRGBA(const glm::vec4 color)
+	{
+		uint8_t r = uint8_t(color.r * 255.0f);
+		uint8_t g = uint8_t(color.g * 255.0f);
+		uint8_t b = uint8_t(color.b * 255.0f);
+		uint8_t a = uint8_t(color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+
+		return result;
+	}
+}
+
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -23,26 +39,31 @@ void Renderer::render()
 {
 	//render every pixel
 
+	float aspectRatio = m_FinalImage->GetWidth() / (float)m_FinalImage->GetHeight();
+
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth() ; x++)
 		{
 			glm::vec2 coord = { x / (float)m_FinalImage->GetWidth() ,y / (float)m_FinalImage->GetHeight() };
-			coord = coord * 2.0f - 1.0f;//-1---1
 
-			m_imageData[x + y * m_FinalImage->GetWidth()] = Perpixel(coord);
-			
+			coord = coord * 2.0f - 1.0f;//-1---1
+			glm::vec4 color = Perpixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+
+			m_imageData[x + y * m_FinalImage->GetWidth()] = utils::ConvertToRGBA(color);
 		}
 	}
 	m_FinalImage->SetData(m_imageData);
 }
 
-uint32_t Renderer::Perpixel(glm::vec2 coord)
+glm::vec4 Renderer::Perpixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
 
-	glm::vec3 rayOrigin (0.0f, 0.0f, 2.0f);
+
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
 	glm::vec3 rayDirection = { coord.x, coord.y , -1.0f };
 	//glm::vec3 rayDirection = raySrceen - rayOrigin;
 	float radius = 0.5f;
@@ -53,9 +74,21 @@ uint32_t Renderer::Perpixel(glm::vec2 coord)
 
 	float dis = b * b - 4.0f * a * c;
 
-	if (dis >= 0.0f)
-		return 0xffff00ff;
+	if (dis < 0.0f)
+		return glm::vec4(0, 0, 0, 1);
 
+	float t0 = (-b + glm::sqrt(dis)) / (2.0f * a);
+	float closeT = (-b - glm::sqrt(dis)) / (2.0f * a);
 
-	return 0xff000000 ;
+	glm::vec3 hitpoint = rayOrigin + rayDirection * closeT;
+	glm::vec3 normal =glm::normalize(hitpoint);
+
+	glm::vec3 lightdirection =glm::normalize(glm::vec3(-1.0f, -1.0f, -1.0f));
+
+	float d = glm::max(glm::dot(normal, -lightdirection), 0.0f);
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= d;
+	return glm::vec4(sphereColor, 1);
+
 }
